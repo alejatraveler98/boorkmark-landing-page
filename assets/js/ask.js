795aba40063f54cbc5f2db23bbe1f7ed56ef91dd
Alejandro Ramírez Gómez <alrg1720@gmail.com>
2020-09-18 22:06:23 -0500

    Responsive Desktop terminado
    Se termino todo el responsive, se tuvieron que modificar muchas
    cuestiones que ya se tenían en Mobile debido a que afectaban en las
    vistas de Desktop, solo quedó pendiente lo de validar un formulario

--- assets/js/ask.js
+++ assets/js/ask.js
@@ -1,10 +1,17 @@
 const ask = document.querySelectorAll('.ask-section__title')
+const imgArrow= document.querySelectorAll('.ask-section__image')
 const paragraph = document.getElementById('ask-1')
 ask.forEach((e)=>{
     e.addEventListener('click', (e)=>{
         const a = e.currentTarget
         const p = a.nextElementSibling
         p.classList.toggle('show-paragraph')
+        a.lastElementChild.classList.toggle('arrow-rotate')
     })
 })
 
+// const arrow = () => {
+//     imgArrow.forEach(e=>{
+//         e.classList.toggle('arrow-rotate')
+//     })    
+// }
